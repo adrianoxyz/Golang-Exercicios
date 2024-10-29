0a243d0a11d1d65502b1dad761edf951fa0af3bd
adrianoxyz <adrianoxyz@gmail.com>
2024-10-29 11:14:23 -0300

    Add tipos e valores

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go Essentials!")
+	fmt.Println("Hello, Go Essentials!") //String
+
+	fmt.Println(2) //Inteiros
+
+	fmt.Println(2.2) //Float
+
+	fmt.Println(2 + 5) //Expressões
+
+	// Booleanos
+	fmt.Println(true)
+	fmt.Println(false)
 }
 
 // Pasta == folders podem ter múltiplos arquivos.
