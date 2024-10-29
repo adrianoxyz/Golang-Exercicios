86af3ce7343798aa2f1ac42146da043a05ecc4c6
adrianoxyz <adrianoxyz@gmail.com>
2024-10-29 10:54:14 -0300

    Curso de Golang

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go Essentials!")
+}
+
+// Pasta == folders podem ter múltiplos arquivos.
+// Pasta pode ter múltiplas funções dentro dela (espalhada em múltiplos arquivos)
+// Toda pasta é um pacote
+// Funções pertencem a um pacote
