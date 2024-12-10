5e9a77316ed0fd9b4ccb992a3d5ffabf5710366a
adrianoxyz <adrianoxyz@gmail.com>
2024-12-09 21:25:30 -0300

    Exercicios Golang

---
+++ estrutura_sequencial/exercicio11/exercicio11.go
@@ -0,0 +1,18 @@
+// Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+// o produto do dobro do primeiro com metade do segundo .
+// a soma do triplo do primeiro com o terceiro.
+// o terceiro elevado ao cubo.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1, num2 float64
+	fmt.Println("Digite o número 1: ")
+	fmt.Scanln(&num1)
+	fmt.Println("Digite o segundo número: ")
+	fmt.Scanln(&num2)
+
+}
